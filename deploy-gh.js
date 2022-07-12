1a368dd217dc59e9ee485fa7a487b68eedde66dd
veprogames <veprogames.gamedev@gmail.com>
2022-07-12 20:44:28 +0200

    gh-pages.js: configure remote

--- deploy-gh.js
+++ deploy-gh.js
@@ -2,7 +2,7 @@ const ghpages = require("gh-pages");
 
 ghpages.publish("public", {
     branch: "gh-pages",
-    repo: "", //fill out later
+    repo: "https://github.com/veprogames/svelte-github-pages-test.git",
     user: {
         name: "veprogames",
         email: "veprogames.gamedev@gmail.com",
